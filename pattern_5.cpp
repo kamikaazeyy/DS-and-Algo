c9892e43136c15038133c52765ee2b358224179a
Pranav Mehrotra <73419557+kamikaazeyy@users.noreply.github.com>
2022-06-20 23:35:02 +0530

    Create pattern_5.cpp

---
+++ pattern_5.cpp
@@ -0,0 +1,40 @@
+//to print the following pattern
+/*
+		*	
+	*	*	*	
+*	*	*	*	*	
+	*	*	*	
+		*	
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int nspaces = n / 2;
+    int nstars = 1;
+    
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= nspaces; j++){
+            cout << ("\t");
+        }
+
+        for (int j = 1; j <= nstars; j++){
+            cout << ("*\t");
+        }
+
+        if (i <= n / 2){
+            nspaces--;
+            nstars += 2;
+        }else{
+            nspaces++;
+            nstars -= 2;
+        }
+
+        cout << endl;
+    }
+}
